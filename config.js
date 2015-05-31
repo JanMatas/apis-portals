f156eb3bc726a41620c8850d9b49ac72b14d7b40
JanMatas <jm6214@ic.ac.uk>
2015-05-31 13:39:29 +0200

    Heroku update

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
 	port: 3000,
 	db: {
 		connectionLimit : 100,
-		host :'localhost',
-		user :'root',
-		password: '092468',
-		database: 'apis_portals',
+		host :'us-cdbr-iron-east-02.cleardb.net',
+		user :'b369ac359b0a33',
+		password: '1839ba5b',
+		database: 'heroku_73648698f572409',
 		debug:false
 	}
 }
