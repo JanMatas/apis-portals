82f43bc93f43c823b09abf601dfe367ac9679746
JanMatas <jm6214@ic.ac.uk>
2015-05-31 14:08:43 +0200

    Another try

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var config = require('./config')
 
-app.set('port', process.env.PORT || 3000);
+var port = process.env.PORT || 5000;
 
 //Middleware
 app.use(require('./auth')); //Authetification middleware
@@ -22,6 +22,6 @@ app.use('/api/map', require('./controllers/api/map')); //Endpoint for map view -
 app.use('/api/transaction', require('./controllers/api/transactionPortal')); //Endpoint for saving transactions
 app.use('/api/positionInfo', require('./controllers/api/positionInfo')); // Endpoint for position of employees
 
-var server = app.listen(app.get('port'), function() { //Starts the app on given port
-    console.log('Server listening on', config.port)
+var server = app.listen(port, function() { //Starts the app on given port
+    console.log('Server listening on', port)
 });
